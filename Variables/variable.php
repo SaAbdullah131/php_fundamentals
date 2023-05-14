ac2422dfcaccc80fddccc825fb8bb8573b495e60
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-05-15 04:02:56 +0600

    exploring php and it's fundamentals

---
+++ Variables/variable.php
@@ -0,0 +1,14 @@
+// Learning and Exploring Php .
+# Exploring php ,so far nothing done by using it
+
+/*
+Learning Php fundamentals of wordpress support engineer and wordpress plugin developer 
+*/
+
+<?php
+// You can also use comments to leave out parts of a code line
+$x = 5 /* + 15 */ + 5;
+$name = 'SA Abdullah'; 
+echo $name;
+echo $x;
+?>
