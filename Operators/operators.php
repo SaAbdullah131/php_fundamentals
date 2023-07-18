31241792968e7fb88d3e478039c04c1164dc8e68
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-07-18 23:57:52 +0600

    all types of php used operators

---
+++ Operators/operators.php
@@ -0,0 +1,18 @@
+<?php
+    /**
+     * PHP Divides the operators in the following groups:
+     * 1.Arithmetic operators
+     * 2.Assignment operators.
+     * 3.Comparison operators
+     * 4.Increment/Decrement operators
+     * 5.Logical operators
+     * 6.String operators
+     * 7.Array operators
+     * 8.conditional assignment operators.
+     *
+     */
+    $x=2;
+    $y=5;
+    $result = $x**$y;// exponentiation  where raising $x to the y'th power
+    echo $result;
+?>
