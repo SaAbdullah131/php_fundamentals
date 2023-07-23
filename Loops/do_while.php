4b40002a1118a63cc80280ef3139bc4c6d7ca9dd
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-07-24 00:00:28 +0600

    do while

---
+++ Loops/do_while.php
@@ -0,0 +1,17 @@
+<?php 
+    $x = 1;
+    do{
+        echo " The number is : $x <br>";
+        $x++;
+    }
+    while($x<=10);
+
+    // just run one times 
+
+    $start = 6;
+    do{
+        echo " The number is : $start <br>";
+        $start++;
+    }
+    while($start <=5); // before checking this condition do while execute do portion always
+?>
---
+++ Loops/while.php
@@ -0,0 +1,12 @@
+<?php
+    $x = 1;
+    while($x<=5){
+        echo "The number is: $x <br>";
+        $x++;
+    }
+    $y=0;
+    while($y<=10){
+        echo "The number is: $x <br>";
+        $y++;
+    }
+?>
